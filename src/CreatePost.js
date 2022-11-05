4d26955e46e9d5eef36250e0915d911a257161b2
micheci <michecimartinez@gmail.com>
2022-11-05 17:13:22 -0500

    Added files and updated mongodo connection

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useNavigate } from "react-router-dom";
-
+import { useState } from 'react';
+import axios from 'axios'
 
 const CreatePost = () => {
     const navigate = useNavigate();
+    const [post,setPost]=useState({
+        title:'',
+        description:'',
+    });
+
+    const handleClick=(e)=>{
+        e.preventDefault();
+        axios.post("/create",post)
+        .then(res=>console.log(res))
+        .catch((err)=>console.log(err));
+    }
+
+    const handleChange=(e)=>{
+        const {name,value}=e.target;
+        setPost((prev)=>{
+            return{
+                ...prev,
+                [name]:value,
+            }
+        });}
+    
+
+    useEffect(()=>{
+        console.log(post)
+    },[post])
+
+
   return (
     <div style={{textAlign:"center"}}>
-    <div >Create a Post</div>afaf
+    <div >Create a Post</div>
 
     <Form>
         <Form.Group>
-            <Form.Control  style={{marginBottom:"1rem"}} name="title" placeholder='Title'/>
-            <Form.Control style={{marginBottom:"1rem"}} name="description" placeholder="Description"/>
+            <Form.Control  value={post.title} onChange={handleChange} style={{marginBottom:"1rem"}} name="title" placeholder='Title'/>
+            <Form.Control value={post.description} style={{marginBottom:"1rem"}} name="description" placeholder="Description" onChange={handleChange}/>
         </Form.Group>
+        <Button onClick={handleClick}></Button>
     </Form>
 
     <Button onClick={() => navigate("/")}>Back</Button>
